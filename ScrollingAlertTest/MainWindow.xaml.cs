f116d1756df616201e78bdbdb17146a77b1deb8e
MoonMan42 <34987599+MoonMan42@users.noreply.github.com>
2020-10-19 16:34:41 -0400

    Added styles and exit button

---
+++ ScrollingAlertTest/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScrollingAlertTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayMessage displayMessage = new DisplayMessage(UserInput_Text.Text);
+            displayMessage.Show();
+        }
+    }
+}
